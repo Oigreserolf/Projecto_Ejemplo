385334810b3f8f3816432a7b02775f654159886d
Oigreserolf <oigreserolf@hotmail.com>
2022-04-09 14:17:00 -0500

    Nuevo programa

---
+++ main.C
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+	printf("Hola mundo/n Ya sé como hacer cambios con git y github\n\n");
+	printf("\t@oigreserolf");
+	return 0;
+	system ("pause");
+}
