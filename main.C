30046df6008459d905f73ca1a431f56413bb8e23
Oigreserolf <oigreserolf@hotmail.com>
2022-04-09 17:01:07 -0500

    Se agrego un comentario y se provo el código desde la terminal de linux

--- main.C
+++ main.C
@@ -4,7 +4,8 @@
 int main ()
 {
 	printf("Hola mundo/n Ya sé como hacer cambios con git y github\n\n");
-	printf("\t@oigreserolf");
+	printf("\t@oigreserolf\n");
+	printf("Se agrega un nuevo print para comentarios");
 	return 0;
 	system ("pause");
 }
